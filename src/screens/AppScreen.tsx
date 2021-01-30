4222ae76d1d23aa5daa1b20c5b72267b9d1afaf5
d-saydin <d-saydin@fintek.local>
2021-01-30 18:58:19 +0300

    integrated rnelements and added header

--- src/screens/AppScreen.tsx
+++ src/screens/AppScreen.tsx
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
-import { Text, Button } from "react-native";
+import { Text } from "react-native";
 
 import { connect } from "react-redux";
 import { NavigationInjectedProps } from "react-navigation";
 import { TODO_ACTION } from "../store/actions/todoAction";
+import { Button, ThemeProvider, Header } from "react-native-elements";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Icon } from "react-native-elements";
 
 interface Props {
   count: number;
@@ -11,18 +14,35 @@ interface Props {
   decreaseCounter(): void;
 }
 
+const theme = {
+	Button: {
+	  raised: true,
+	  titleStyle: {
+			color: "red",
+	  },
+	},
+};
+
 class AppScreen extends Component<NavigationInjectedProps & Props> {
 	render() {
 		return (
-			<>
-				<Text>{this.props.count}</Text>
-				<Button title={"+"} onPress={() => this.props.increaseCounter()} />
-				<Button
-					title={"Home"}
-					onPress={() => this.props.navigation.navigate("Home")}
-				/>
-				<Button title={"-"} onPress={() => this.props.decreaseCounter()} />
-			</>
+			<SafeAreaProvider>
+				<ThemeProvider theme={theme}>
+					<Header
+						placement="left"
+						leftComponent={{ icon: "menu", color: "#fff" }}
+						centerComponent={{ text: "MY TITLE", style: { color: "#fff" } }}
+						rightComponent={{ icon: "home", color: "#fff" }}
+					/>
+					<Text>{this.props.count}</Text>
+					<Button title={"+"} onPress={() => this.props.increaseCounter()} />
+					<Button
+						title={"Home"}
+						onPress={() => this.props.navigation.navigate("Home")}
+					/>
+					<Button title={"-"} onPress={() => this.props.decreaseCounter()} />
+				</ ThemeProvider>
+			</SafeAreaProvider>
 		);
 	}
 }
